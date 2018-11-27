ef450a380754fe3792b2082a7cfe54e26dfd4525
Michael Martins <martins.m09@hotmail.com>
2018-11-27 09:48:43 -0200

    Infogenda - v1.0.0
    - Criação do Projeto inicial
    - Organização dos diretórios

---
+++ Infogenda/app/src/main/java/br/com/development/infogenda/controllers/Inicio.java
@@ -0,0 +1,23 @@
+package br.com.development.infogenda.controllers;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import br.com.development.infogenda.R;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
